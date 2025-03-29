6fe4d55587845e04da97857412af4c46616abf42
Shanu Aaryan <shanuaaryan8@gmail.com>
2025-03-29 22:16:48 +0530

    updated use-fetch directory

--- app/(main)/interview/_components/quiz.jsx
+++ app/(main)/interview/_components/quiz.jsx
@@ -14,7 +14,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { generateQuiz, saveQuizResult } from "@/actions/interview";
 import QuizResult from "./quiz-result";
-import useFetch from "@/app/hooks/use-fetch";
+import useFetch from "../../../hooks/use-fetch";
 import { BarLoader } from "react-spinners";
 
 export default function Quiz() {
